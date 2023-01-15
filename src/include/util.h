97c2c125892469b0eab801e16a5b642c0886c6e1
Dawen <darwinyang0805@gmail.com>
2023-01-15 14:51:57 +1100

    save more initial work

---
+++ src/include/util.h
@@ -0,0 +1,10 @@
+//
+// Created by Dawen on 1/6/23.
+//
+
+#ifndef CPP_SERVER_UTIL_H
+#define CPP_SERVER_UTIL_H
+
+void errif(bool, const char*);
+
+#endif //CPP_SERVER_UTIL_H
---
+++ src/util.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Dawen on 1/6/23.
+//
+
+#include <cstdio>
+#include <cstdlib>
+#include "util.h"
+
+void errif(bool condition, const char *errmsg) {
+    if(condition) {
+        perror(errmsg);
+        exit(EXIT_FAILURE);
+    }
+}
